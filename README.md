b6e93c384afeaddf30f452fdef92a1c75abbf635
Mohammad Fares <faressoft.com@gmail.com>
2017-05-07 13:38:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Subhah
+
+[![npm](https://img.shields.io/npm/v/mysql-punisher.svg)](https://www.npmjs.com/package/subhah)
+[![npm](https://img.shields.io/npm/l/mysql-punisher.svg)](https://github.com/faressoft/subhah/blob/master/LICENSE)
+
+A simple subhah counter for muslims geeks to be used in the terminal. This is just a simple dummy script that I created for personal use at first, I would like to share it with you.
+
+# Installation
+
+To use the Subhah from you command line interface, you have to:
+
+```
+npm install subhah -g
+```
+
+### Usage
+
+Just run it by:
+
+```
+subhah
+```
+
+Keys
+
+* **Enter**: to increment the counter.
+* **Backspace**: to reset the counter.
+
+# License
+
+This project is under the MIT license.
---
+++ bin/index.js
@@ -0,0 +1,45 @@
+/**
+ * Subhah Counter
+ * 
+ * @author Mohammad Fares <faressoft.com@gmail.com>
+ */
+
+var keypress = require('keypress')
+  , counter  = 0;
+
+keypress(process.stdin);
+
+/**
+ * When the user press a key
+ */
+process.stdin.on('keypress', function (ch, key) {
+
+  // The enter key
+  if (key.name == 'return') {
+
+    // Clear the window
+    process.stdout.write('\x1B[2J\x1B[0f\u001b[0;0H');
+    // Increment the counter and print it
+    console.log(++counter);
+
+  }
+
+  // The backspace key
+  if (key.name == 'backspace') {
+
+    // Clear the window
+    process.stdout.write('\x1B[2J\x1B[0f\u001b[0;0H');
+    // Reset the counter and print it
+    console.log(counter = 0);
+
+  }
+
+  // CTRL + C key
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
